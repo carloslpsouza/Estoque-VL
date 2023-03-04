5f0dd3b9a301a4bb8c71493f322b4c368bafd93d
Carlos Souza <carlos.souza@unigranrio.br>
2023-03-04 16:18:01 -0300

    Construção sidebar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,8 +10,10 @@
     <!-- css bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Local styles -->
+    <link href="/css/sidebars.css" rel="stylesheet">
     <link href="/css/styles.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
 </head>
@@ -23,7 +25,97 @@
                 <p class="msg">{{ session('msg') }}</p>
             @endif
         </div>
-        @yield('content')
+
+        {{-- nav --}}
+
+        <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="bootstrap" viewBox="0 0 118 94">
+                <title>Bootstrap</title>
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z">
+                </path>
+            </symbol>
+            <symbol id="home" viewBox="0 0 16 16">
+                <path
+                    d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
+            </symbol>
+            <symbol id="table" viewBox="0 0 16 16">
+                <path
+                    d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 2h-4v3h4V4zm0 4h-4v3h4V8zm0 4h-4v3h3a1 1 0 0 0 1-1v-2zm-5 3v-3H6v3h4zm-5 0v-3H1v2a1 1 0 0 0 1 1h3zm-4-4h4V8H1v3zm0-4h4V4H1v3zm5-3v3h4V4H6zm4 4H6v3h4V8z" />
+            </symbol>
+            <symbol id="market" viewBox="0 0 16 16">
+                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+            </symbol>
+        </svg>
+
+        <main class="d-flex flex-nowrap">
+            <div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
+                <a href="/"
+                    class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+                    <svg class="bi pe-none me-2" width="30" height="24">
+                        <use xlink:href="#market" />
+                    </svg>
+                    <span class="fs-5 fw-semibold">Estoque VL 1.0</span>
+                </a>
+                <ul class="list-unstyled ps-0">
+                    <li class="mb-1">
+                        <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
+                            data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+                            Produtos
+                        </button>
+                        <div class="collapse show" id="home-collapse">
+                            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                <li><a href="/produto/cadastro"
+                                        class="link-dark d-inline-flex text-decoration-none rounded">Cadastrar</a></li>
+                                <li><a href="#"
+                                        class="link-dark d-inline-flex text-decoration-none rounded">Retirar</a></li>
+                                <li><a href="#"
+                                        class="link-dark d-inline-flex text-decoration-none rounded">Devolver</a></li>
+                                <li><a href="/lista"
+                                        class="link-dark d-inline-flex text-decoration-none rounded">Listar</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="mb-1">
+                        <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
+                            data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+                            Fornecedores
+                        </button>
+                        <div class="collapse" id="dashboard-collapse">
+                            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                <li><a href="/fornecedor/cadastro"
+                                        class="link-dark d-inline-flex text-decoration-none rounded">Cadastrar</a></li>
+                                <li><a href="/fornecedores"
+                                        class="link-dark d-inline-flex text-decoration-none rounded">Listar</a></li>
+                                <li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="border-top my-3"></li>
+                    <li class="mb-1">
+                        <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
+                            data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+                            Account
+                        </button>
+                        <div class="collapse" id="account-collapse">
+                            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                <li><a href="#"
+                                        class="link-dark d-inline-flex text-decoration-none rounded">Sign out</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="flex-shrink-0 p-3 bg-white">
+                @yield('content')
+            </div>
+        </main>
+
+        <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="/js/sidebars.js"></script>
+        {{-- /nav --}}
+
+        {{--  --}}
     </div>
 </body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.main')
 @section('title', 'Welcome')
 @section('content')
-<div id="produtos-conteiner" class="col-md-10 offset-md-1">
-    <h3>Estoque VL - 1.0</h3>
+<div id="produtos-conteiner" class="col-md-1">
     <h4>Painel</h4>
     <div class="list-group">
         <a href="/lista" class="list-group-item d-flex justify-content-between align-items-center">
@@ -24,21 +23,6 @@
         <a href="/movimentos" class="list-group-item d-flex justify-content-between align-items-center">
             Movimentos
             <span class="badge bg-primary rounded-pill">{{$contadores['qtdeMovimentos']}}</span>
-        </a>
-    </div>
-    <h4>Atalhos</h4>
-    <div class="list-group">
-        <a href="/produto/cadastro" class="list-group-item d-flex justify-content-between align-items-center">
-            Cadastrar Produtos
-        </a>
-        <a href="/fornecedor/cadastro" class="list-group-item d-flex justify-content-between align-items-center">
-            Cadastrar Fornecedores
-        </a>
-        <a href="/estoque" class="list-group-item d-flex justify-content-between align-items-center">
-            Entrada de produtos
-        </a>
-        <a href="/estoque" class="list-group-item d-flex justify-content-between align-items-center">
-            Saída de produtos
         </a>
     </div>
 </div>
